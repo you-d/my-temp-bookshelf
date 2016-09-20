b894d56f725f726a0919ec7ddc9536d70430a3b9
Yudiman Kwanmas <Yudiman Kwanmas>
2016-09-20 14:12:27 +1000

    Heroku Friendly - Dynamic Port

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ var option_1 = {
     devFlagPlugin
   ],
   url: {
-    host: process.env.HOST.toString() || 'localhost',
+    host: 'https://my-temp-bookshelf.herokuapp.com' || 'localhost',
     port: parseInt(process.env.PORT) || 3000
   }
 }
