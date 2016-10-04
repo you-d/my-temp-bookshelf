e6362d13b70d10cbd80caeec1b79d23dcb33a36f
Yudiman Kwanmas <Yudiman Kwanmas>
2016-10-04 18:47:27 +1100

    Set Remote Database URI

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
 module.exports = {
     devUrl: 'mongodb://localhost:27017/my-temp-bookshelf',
-    prodUrl: 'mongodb://localhost:27017/my-temp-bookshelf',
-    sessionSecret: ''
+    prodUrl: 'mongodb://heroku_69g6g74r:pavsomejna3fb58ldfpetrskcs@ds049446.mlab.com:49446/heroku_69g6g74r'
 }
